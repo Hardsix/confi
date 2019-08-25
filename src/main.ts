9e98ff868569d02990a9723e4f1bff2b563a891d
Tomislav Bradaric <the.tomislav.bradaric@gmail.com>
2019-08-25 16:13:00 +0200

    Hardcode vars in docker-compose.yml as docker insists on using only .env for build-time variables, no matter what env_file is set to (seems that's only used in runtime). Use @nestjs/swagger

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { createConnection } from 'typeorm';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 import { AppConfig } from './shared/config/config';
 
@@ -7,7 +8,19 @@ async function bootstrap() {
   await createConnection();
 
   const app = await NestFactory.create(AppModule);
+
+  const options = new DocumentBuilder()
+    .setTitle('Confi')
+    .setDescription('Confi API description')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('/', app, document);
+
   await app.listen(AppConfig.appPort);
+  
+  console.log(`Listening at ${AppConfig.appPort}`);
 }
 
 bootstrap();
