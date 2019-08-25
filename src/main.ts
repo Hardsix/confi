823eeeea9ef8bdd66fdcd6d2198ddd1005c8b5ee
Tomislav Bradaric <the.tomislav.bradaric@gmail.com>
2019-08-25 11:28:24 +0200

    Implement env variable support

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { AppConfig } from './shared/config/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen(AppConfig.appPort);
 }
 
 bootstrap();
---
+++ src/shared/config/config.ts
@@ -0,0 +1,5 @@
+require('dotenv/config');
+
+export const AppConfig = {
+  appPort: process.env.APP_PORT,
+}
