500bdbcfb2173f7ca69f4582c6289d17b67b9406
Tomislav Bradaric <the.tomislav.bradaric@gmail.com>
2019-08-25 11:14:17 +0200

    Take initial steps to modularize architecture

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { HelloController } from './hello.module/hello.controller';
+import { HelloService } from './hello.module/hello.service';
 
 @Module({
   imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [HelloController],
+  providers: [HelloService],
 })
+
 export class AppModule {}
